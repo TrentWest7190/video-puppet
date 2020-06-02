3dd7e0d4a41879dd97e5e0fd524fb4be75f854aa
Trent West <trentwest7190@gmail.com>
2020-06-01 19:17:30 -0500

    Updates

---
+++ precache-manifest.8ae57907516d012c8ea7205f9ea58794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487821ef3e6978c56c0be684ca0d42af",
+    "url": "/video-puppet/index.html"
+  },
+  {
+    "revision": "480d6daad6f5cef36571",
+    "url": "/video-puppet/static/css/main.2df6bf4b.chunk.css"
+  },
+  {
+    "revision": "e80ad0b135bd05f9e3cc",
+    "url": "/video-puppet/static/js/2.ddfa5fc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-puppet/static/js/2.ddfa5fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "480d6daad6f5cef36571",
+    "url": "/video-puppet/static/js/main.6321ba12.chunk.js"
+  },
+  {
+    "revision": "a7e435cc693411e8f7ea",
+    "url": "/video-puppet/static/js/runtime-main.2e899a95.js"
+  },
+  {
+    "revision": "92e0ca15fd3cc1cbfcf1dcebd9b7eedd",
+    "url": "/video-puppet/static/media/mouth2.92e0ca15.mp4"
+  }
+]);
